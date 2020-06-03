486bace42f073cd5a618d58f1bd379357b049b60
Raj-1703 <63633015+Raj-1703@users.noreply.github.com>
2020-06-03 13:02:15 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# chat_room
-CLI internet chat room for multiple clients
+Used Python and Socket to build this cli chat room.
+It has one server with infinite clients.
+In case of more than one clients, it gives reply to the message which appeared first on the server.
---
+++ server.py
@@ -0,0 +1,69 @@
+import socket
+import random
+import datetime
+from _thread import *
+
+# Creating the socket
+try:
+    print("Creating the Socket....")
+    s = socket.socket()
+    print("Socket created successfully.")
+except socket.error as err:
+    print("Socket creation failed due to " + str(err))
+
+#defining host
+host = socket.gethostname()
+print("Host: ", host)
+
+#defining port
+port = 1234
+
+ThreadCount = 0
+
+# Function defined to select a random line from messages.txt file.
+def random_line(fname):
+    lines = open(fname).read().splitlines()
+    return random.choice(lines)
+
+def dt():
+    now = datetime.datetime.now()
+    return(now.strftime("%Y-%m-%d %H:%M:%S"))
+
+try:
+    print("Binding the Socket....")
+    s.bind((host, port))
+    print("Socket binded successfully.")
+except socket.error as err:
+    print("There is some problem resolving the host " + str(err))
+    quit()
+
+print('Waitiing for a Connection..')
+s.listen(100)
+
+def threaded_client(c):
+    message = "Welcome to the Server."
+    message = message.encode('utf-8')
+    c.send(message)
+    while True:
+        incoming_message = c.recv(1024)
+        incoming_message = incoming_message.decode('utf-8')
+        if len(str(incoming_message)) > 0:
+            print(dt())
+            print("Client " + "(" + address[0] + ":" + str(address[1]) + ")" + ": " +  incoming_message)
+            print("message received")
+        else:
+            break
+        sending_message = str(random_line('messages.txt'))
+        print("You: ", sending_message)
+        sending_message = sending_message.encode('utf-8')
+        c.send(sending_message)
+        print("message sent")
+    c.close()
+
+while True:
+    c, address = s.accept()
+    print('Connected to: ' + address[0] + ':' + str(address[1]))
+    start_new_thread(threaded_client, (c, ))
+    ThreadCount += 1
+    print('Thread Number: ' + str(ThreadCount))
+s.close()
